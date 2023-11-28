ad1d6b9853f96330a73a815cd3dd80a606d4b6eb
Aastik Yadav <aastikyadav1997.ay@gmail.com>
2023-11-28 13:49:41 +0530

    About section completed

--- src/components/canvas/Computers.tsx
+++ src/components/canvas/Computers.tsx
@@ -49,7 +49,7 @@ const ComputerCanvas = ()=>{
 
   return (
     <Canvas frameloop="demand" shadows dpr={[1,2]} camera={{position: [20, 3, 5], fov: 25}} gl={{preserveDrawingBuffer: true}}>
-      <Suspense fallback={null}>
+      <Suspense fallback={<CanvasLoader/>}>
         <OrbitControls enableZoom={false} maxPolarAngle={Math.PI/2} minPolarAngle={Math.PI/2}/>
         <Computers isMobile={isMobile}/>
       </Suspense>
