693ed75d70fc884bb827b4488d65149084cc734c
Aastik Yadav <aastikyadav1997.ay@gmail.com>
2023-11-28 21:24:58 +0530

    Removed unnecessary comments

--- src/components/canvas/Stars.tsx
+++ src/components/canvas/Stars.tsx
@@ -2,7 +2,6 @@ import { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import {random} from 'maath';
-// import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = () => {
 
