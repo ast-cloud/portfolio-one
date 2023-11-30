74407cf15eedafe413e3fb90960f8cf1c8c39137
Aastik Yadav <aastikyadav1997.ay@gmail.com>
2023-12-01 00:10:25 +0530

    Changed technology hover animations

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -6,10 +6,7 @@ const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology)=>(
-        <motion.div initial={{scale: 0.8, opacity: 0}} animate={{scale: 1, opacity: 1, transition:{delay: 0.8}}} whileHover={{scale: 1.2, transition:{delay: 0}}} variants={{
-          hidden: {scale: 0.8, opacity: 0},
-          visible: {scale: 1, opacity: 1, transition:{delay: 0.4}}
-        }}>
+        <motion.div animate={{scale: 1}} whileHover={{scale: 1.2}}>
           <div className="flex flex-col items-center" key={`tech-${technology.name}`}>
             <div className='w-28 h-28 bg-white flex justify-center rounded-full p-4' key={technology.name}>
               <img src={technology.icon} alt={technology.name} />
