c56e290991e532d9541320e05ab0540a1778cd51
Aastik Yadav <aastikyadav1997.ay@gmail.com>
2023-11-30 16:57:28 +0530

    Removed ball canvas from technologies

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,4 +1,3 @@
-import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from '../constants';
 
@@ -7,8 +6,8 @@ const Tech = () => {
     <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology)=>(
         <div className="flex flex-col items-center" key={`tech-${technology.name}`}>
-          <div className='w-28 h-28' key={technology.name}>
-            <BallCanvas icon={technology.icon} />
+          <div className='w-28 h-28 bg-white flex justify-center rounded-full p-4' key={technology.name}>
+            <img src={technology.icon} alt={technology.name} />
           </div>
           <p className="mt-2 text-white font-bold">{technology.name}</p>
         </div>
--- src/components/canvas/index.ts
+++ src/components/canvas/index.ts
@@ -1,6 +1,5 @@
 import EarthCanvas from "./Earth";
-import BallCanvas from "./Ball";
 import ComputerCanvas from "./Computers";
 import StarsCanvas from "./Stars";
 
-export { EarthCanvas, BallCanvas, ComputerCanvas, StarsCanvas };
+export { EarthCanvas, ComputerCanvas, StarsCanvas };
